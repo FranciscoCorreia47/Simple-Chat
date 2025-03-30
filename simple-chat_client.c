a1672b91a7070555f2c3cf3456dd50258c7a02ce
RicLps <armindoricardo656@gmail.com>
2025-03-30 22:41:34 -0100

    Update simple-chat_client.c

--- simple-chat_client.c
+++ simple-chat_client.c
@@ -1,9 +1,13 @@
 #include "simple-chat_functions.h"
 
+char buff[MAX_MSG_SIZE];
+char message[MAX_MSG_SIZE];
+int buff_bytes = 0;
+
+DWORD WINAPI receive_messages(LPVOID socketFileDescriptor);
+
 int main(void) {
-
-	char message[MAX_MSG_SIZE];
-
+	
 	char* ip = "127.0.0.1";
 	WSADATA wsa;
 	WSAStartup(MAKEWORD(2, 0), &wsa);
@@ -18,15 +22,21 @@ int main(void) {
 		printf("Connection was successful!\n");
 	else
 		printf("Connection error!\n");
+
+	HANDLE thread = CreateThread(NULL, 0, receive_messages, (LPVOID)&socketFileDescriptor, 0, NULL);
+	while(1){
+		printf("> ");
+		scanf("%s", message);
+		send(socketFileDescriptor, message, strlen(message), 0);
+	}
 	
-	printf("> ");
-	scanf("%s", message);
-	send(socketFileDescriptor, message, strlen(message), 0);
-	recv(socketFileDescriptor, message, MAX_MSG_SIZE, 0);
-
-	printf("Response: %s\n", message);
-
 	closesocket(socketFileDescriptor);
+	return 0;
+}
 
+DWORD WINAPI receive_messages(LPVOID socketFileDescriptor){
+	while((bytes_read = recv(socketFileDescriptor, buff, sizeof(buff), 0) > 0){
+		printf("Received: %s", buff);
+	}
 	return 0;
 }
