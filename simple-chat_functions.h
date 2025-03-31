5b7e2aab630c29592df96f12c81c248c3d4e11f5
47Francisco_Correia <jfranciscosacorreia@gmail.com>
2025-03-31 19:57:04 -0100

    Added the files from the local repo on my machine

---
+++ simple-chat_functions.h
@@ -0,0 +1,22 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+
+#include <WinSock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
+#include <malloc.h>
+
+#pragma comment(lib, "WS2_32.lib")
+
+#define MAX_MSG_SIZE 512
+#define PORT 50000
+
+SOCKET initialize_Socket_IPv4();
+
+struct sockaddr_in generate_IPv4_Address(char* ip, int port);
+
