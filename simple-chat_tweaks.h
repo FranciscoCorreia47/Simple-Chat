7dd4e93dd2bc0d4832ad43d1a383f60bd3d1dcd8
47Francisco_Correia <jfranciscosacorreia@gmail.com>
2025-04-16 15:53:42 -0100

    Update simple-chat_tweaks.h

--- simple-chat_tweaks.h
+++ simple-chat_tweaks.h
@@ -1,9 +1,1 @@
-#include "simple-chat_functions.h"
-
-void encrypt(char text[512]){
-  char key[6] = "X̌₽æþ¤";
-  for(int i = 0; i < strlen(text); i++){
-        text[i] = text[i] ^ key[i % strlen(key)];
-  }
-}
-
+void encrypt(char text[512]);
