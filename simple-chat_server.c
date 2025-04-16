e1606d798e0adf7c94f3a90134db0a75adc51732
47Francisco_Correia <jfranciscosacorreia@gmail.com>
2025-04-16 16:13:15 -0100

    Update simple-chat_server.c
    Corrected many things, added threaded functionality. Missing things like accepting other clients and sending received messages to all connected clients.

--- simple-chat_server.c
+++ simple-chat_server.c
@@ -1,45 +1,58 @@
 #include "simple-chat_functions.h"
 
-/*
+void* receive_messages(void* clientSocket);
 
-  The function send_messages should receive the clientFD as a parameter, for internal use
-
-*/
-
-
-DWORD WINAPI send_messages(LPVOID serverFD);
-
-char buff[MAX_MSG_SIZE] = { 0 };
-char message[MAX_MSG_SIZE] = { 0 };
+char buff[MAX_MSG_SIZE];
+pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
 int bytes_read = 0;
+
 
 int main(void) {
 
 	WSADATA wsa;
 	WSAStartup(MAKEWORD(2, 2), &wsa);
+	pthread_t thread1;
 
 	SOCKET server = initialize_Socket_IPv4();
+
 	struct sockaddr_in serverAddress = generate_IPv4_Address("", PORT);
 
 	bind(server, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-  listen(server, 5);
-  
+
+	int checkIP = getWirelessIP();
+	if (checkIP) {
+		Sleep(2000);
+		return 1;
+	}
+
+	listen(server, 5);
+
 	SOCKET client = accept(server, NULL, NULL);
 
-		while((bytes_read = recv(client, buff, MAX_MSG_SIZE, 0)) > 0){
-		printf("Message: %s\n", buff);
-	}
-	HANDLE thread = CreateThread(NULL, send_messages, (LPVOID)&server, 0, NULL);
+	pthread_mutex_init(&print_mutex, NULL);
+	pthread_create(&thread1, NULL, receive_messages, (void*) &client);
+
+	pthread_join(&thread1, NULL);
 
 	closesocket(client);
 	closesocket(server);
 
 	WSACleanup();
-		
+
 	return 0;
 }
 
-DWORD WINAPI send_messages(LPVOID serverFD){
-	send(clientFD, buff, bytes_read, 0);
-	return 0;
+void* receive_messages(void* clientSocket) {
+	SOCKET* client = (SOCKET*)clientSocket;
+	buff[bytes_read] = '\0';
+	while ((bytes_read = recv(*client, buff, MAX_MSG_SIZE, 0)) > 0) {
+		pthread_mutex_lock(&print_mutex);
+		printf("Message: %s\n", buff);
+		fflush(stdout);
+		pthread_mutex_unlock(&print_mutex);
+
+		encrypt(buff);
+		send(*client, buff, bytes_read, 0);
+	}
+	return NULL;
 }
