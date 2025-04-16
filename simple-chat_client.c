849651ba8bb480116d72e55045b17ab3807a3920
47Francisco_Correia <jfranciscosacorreia@gmail.com>
2025-04-16 15:48:03 -0100

    Update simple-chat_client.c
    Stuff is being corrected, specifically with threads. Also added XOR encryption/decryption on clients.

--- simple-chat_client.c
+++ simple-chat_client.c
@@ -1,52 +1,77 @@
 #include "simple-chat_functions.h"
 
-DWORD WINAPI receive_messages(LPVOID socketFileDescriptor);
-
 char buff[MAX_MSG_SIZE];
 char message[MAX_MSG_SIZE];
 int buff_bytes = 0;
+pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* receive_messages(void* serverSocket);
 
 int main(void) {
 
 	WSADATA wsa;
 	WSAStartup(MAKEWORD(2, 2), &wsa);
+	pthread_t thread1;
+	char* serverIp;
 
-	SOCKET client = initialize_Socket_IPv4();
+	printf("Insert the server's IP?");
+	scanf("%s", serverIp);
 
-	struct sockaddr_in client_address = generate_IPv4_Address("127.0.0.1", PORT);
+	SOCKET server = initialize_Socket_IPv4();
 
-	int connectionResult = connect(client, (struct sockaddr*)&client_address, sizeof(client_address));
+	struct sockaddr_in server_address = generate_IPv4_Address(serverIp, PORT);
+
+	/*short int checkPort = checkPort5000();
+	if (checkPort) {
+		Sleep(2000);
+		return 1;
+	}*/
+
+	int connectionResult = connect(server, (struct sockaddr*)&server_address, sizeof(server_address));
 	switch (connectionResult) {
-		case SOCKET_ERROR:
-			wprintf(L"Failed to Connect to the server %ld\n", WSAGetLastError());
-			closesocket(client);
-			WSACleanup();
-			return 1;
-		default:
-			wprintf(L"Connected\n");
-			break;
-  }
-
-	HANDLE thread = CreateThread(NULL, 0, receive_messages, (LPVOID)&socketFileDescriptor, 0, NULL);
-	while(1){
-		printf("> ");
-		scanf("%s", message);
-		send(socketFileDescriptor, message, strlen(message), 0);
+	case SOCKET_ERROR:
+		wprintf(L"Failed to Connect to the server %ld\n", WSAGetLastError());
+		closesocket(server);
+		WSACleanup();
+		return 1;
+	default:
+		wprintf(L"Connected\n");
+		break;
 	}
 
+	pthread_mutex_init(&print_mutex, NULL);
+	pthread_create(&thread1, NULL, *receive_messages, (void *)&server);
+
+	char message[MAX_MSG_SIZE] = { 0 };
+	while(1) {
+
+		memset(message, 0, sizeof(message));
+		pthread_mutex_lock(&print_mutex);
+		printf("Write your message: ");
+		scanf("%s", message);
+		encrypt(message);
+		send(server, message, MAX_MSG_SIZE, 0);
+
+	}
+
+	printf("Bye!\n");
 	Sleep(2000);
 
-	closesocket(client);
+	closesocket(server);
+	pthread_join(thread1, NULL);
 	WSACleanup();
-  
-  return 0;
-}
 
-DWORD WINAPI receive_messages(LPVOID socketFileDescriptor){
-	while((bytes_read = recv(socketFileDescriptor, buff, sizeof(buff), 0)) > 0){
-		printf("Received: %s", buff);
-	}
 	return 0;
 }
 
-
+void* receive_messages(void* serverSocket) {
+	buff[buff_bytes] = '\0';
+	SOCKET* server = (SOCKET*)serverSocket;
+	while ((buff_bytes = recv(*server, buff, sizeof(buff), 0)) > 0) {
+		pthread_mutex_lock(&print_mutex);
+		wprintf(L"Received: %s\n", buff);
+		fflush(stdout);
+		pthread_mutex_unlock(&print_mutex);
+	}
+	return NULL;
+}
