2a51af268c8d56ced91c41244cb0331eb9c1a023
47Francisco_Correia <jfranciscosacorreia@gmail.com>
2025-04-21 15:56:30 -0100

    Update simple-chat_functions.h

--- simple-chat_functions.h
+++ simple-chat_functions.h
@@ -15,12 +15,13 @@
 
 #pragma comment(lib, "WS2_32.lib")
 
+// The default message size
 #define MAX_MSG_SIZE 1024
-#define PORT 50000
+// The network port used by the app
+#define PORT 5000
 
+// Definition of the functions declared on simple-chat_functions.c
 int checkPort5000();
 int getWirelessIP();
 SOCKET initialize_Socket_IPv4();
 struct sockaddr_in generate_IPv4_Address(char* ip, int port);
-
-
