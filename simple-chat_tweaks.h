8a1924fae37947a763831fddd18b19138239c3cf
47Francisco_Correia <jfranciscosacorreia@gmail.com>
2025-04-30 16:04:38 -0100

    flush_input function
    This function does what fflush(stdin) is supposed to do, but in a consistent and safe manner.

--- simple-chat_tweaks.h
+++ simple-chat_tweaks.h
@@ -3,3 +3,8 @@ void encrypt(char text[512]);
 
 //Function that put together the username chosen by the client and the message that will be sent
 void concat(char message[512], char username[30]);
+
+static void flush_input(void){
+  int ch;
+  while ((ch = getchar()) != '\n' && ch != EOF);
+}
