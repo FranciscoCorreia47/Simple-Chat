7d3c54c6c6a1875b22bb1076f5b1776b5ec3d4be
47Francisco_Correia <jfranciscosacorreia@gmail.com>
2025-04-30 16:27:07 -0100

    Update simple-chat_tweaks.h

--- simple-chat_tweaks.h
+++ simple-chat_tweaks.h
@@ -4,6 +4,7 @@ void encrypt(char text[512]);
 //Function that put together the username chosen by the client and the message that will be sent
 void concat(char message[512], char username[30]);
 
+// This Function does what fflush(stdin) should do, without any risks
 static void flush_input(void){
   int ch;
   while ((ch = getchar()) != '\n' && ch != EOF);
