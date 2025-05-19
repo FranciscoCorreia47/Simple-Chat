2ba74cf30f752ff8045b89f97356bade624c4d3b
francisco_correia47 <jfranciscosacorreia@gmail.com>
2025-05-19 15:57:06 -0100

    Update simple-chat_client.c
    Corrected some errors and unsafe memory usage.

--- simple-chat_client.c
+++ simple-chat_client.c
@@ -1,7 +1,7 @@
 #include "simple-chat_functions.h"
 
-char buff[MAX_MSG_SIZE];
-int buff_bytes = 0;
+char						buff[MAX_MSG_SIZE];
+int							buff_bytes  = 0;
 pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* receive_messages(void* serverSocket);
@@ -11,21 +11,14 @@ int main(void) {
 	WSADATA wsa;
 	WSAStartup(MAKEWORD(2, 2), &wsa);
 	pthread_t thread1;
-	char* serverIp;
+	char serverIp[INET_ADDRSTRLEN];
 
-	printf("Insert the server's IP?");
+	printf("Insert the server's IP?\n>");
 	scanf("%s", serverIp);
 
 	SOCKET server = initialize_Socket_IPv4();
 
 	struct sockaddr_in server_address = generate_IPv4_Address(serverIp, PORT);
-
-	/*short int checkPort = checkPort5000();
-	if (checkPort) {
-		Sleep(2000);
-		return 1;
-  
-	}*/
 
 	int connectionResult = connect(server, (struct sockaddr*)&server_address, sizeof(server_address));
 	switch (connectionResult) {
@@ -35,33 +28,34 @@ int main(void) {
 		WSACleanup();
 		return 1;
 	default:
-		wprintf(L"Connected\n");
+		wprintf(L"Connected to server on %s\n", serverIp);
 		break;
 	}
 
 	pthread_mutex_init(&print_mutex, NULL);
-	pthread_create(&thread1, NULL, *receive_messages, (void *)&server);
+	pthread_create(&thread1, NULL, receive_messages, (void *)&server);
 
 	char message[MAX_MSG_SIZE] = { 0 };
 	while(1) {
 
 		memset(message, 0, sizeof(message));
-		pthread_mutex_lock(&print_mutex);
 		
+		pthread_mutex_lock(&print_mutex);
+
 		flush_input();
 
-		printf("Write your message: ");
-
+		printf("> ");
+		
 		if (fgets(message, MAX_MSG_SIZE, stdin)) {
-		  // Strip newline if present
-		  size_t len = strlen(message);
-		  if (len > 0 && message[len - 1] == '\n') {
-		    message[len - 1] = '\0';
-		  }
+			// Strip newline if present
+			size_t len = strlen(message);
+			if (len > 0 && message[len - 1] == '\n') {
+				message[len - 1] = '\0';
+			}
 		}
 
 		fflush(stdout);
-		
+
 		pthread_mutex_unlock(&print_mutex);
 		
 		if (strcmp(message, "/exit") == 0) {
@@ -103,4 +97,3 @@ void* receive_messages(void* serverSocket) {
 	}
 	return NULL;
 }
-
