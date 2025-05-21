17b06dcda888ee025a542f8759093dda8cf6d485
francisco_correia47 <jfranciscosacorreia@gmail.com>
2025-05-21 17:40:03 -0100

    Update simple-chat_tweaks.h
    Made all the functions static because they are all very simple

--- simple-chat_tweaks.h
+++ simple-chat_tweaks.h
@@ -1,11 +1,24 @@
-//Function that encrypts the message from the client before sending it, using XOR
-void encrypt(char text[512]);
+// Encripts messages using XOR decryption
+static void encrypt(char text[512]) {
+    char key[12] = "X̌₽æþ¤";
+    for (int i = 0; i < strlen(text); i++) {
+        text[i] = text[i] ^ key[i % strlen(key)];
+    }
+}
 
-//Function that put together the username chosen by the client and the message that will be sent
-void concat(char message[512], char username[30]);
+// Concatenates the user's name and the message, to ease sending to server and printing on other clients/broadcasting
+static char* concat(char message[512], char username[30]) {
+    char* final_message;
+    strcat(final_message, username);
+    strcat(final_message, "> ");
+    strcat(final_message, message);
 
-// This Function does what fflush(stdin) should do, without any risks
-static void flush_input(void){
-  int ch;
-  while ((ch = getchar()) != '\n' && ch != EOF);
+    // Output should look like: "username> The message that the user inputed"
+    return final_message;
+}
+
+// This function simply does what fflush(stdin) is supposed to do, but in a safe and consistent manner
+static void flush_input(void) {
+	int ch;
+	while ((ch = getchar()) != '\n' && ch != EOF);
 }
