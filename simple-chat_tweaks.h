dab0604449b566b0ab4dce12b51bf692f15ba281
francisco_correia47 <jfranciscosacorreia@gmail.com>
2025-06-24 22:04:02 -0100

    Final update simple-chat_tweaks.h
    At long last, it's done...

--- simple-chat_tweaks.h
+++ simple-chat_tweaks.h
@@ -1,5 +1,5 @@
 // Encripts messages using XOR decryption
-static void encrypt(char text[1024]) {
+static void encrypt(char text[512]) {
     char key[12] = "X̌₽æþ¤";
     for (int i = 0; i < strlen(text); i++) {
         text[i] = text[i] ^ key[i % strlen(key)];
@@ -7,13 +7,19 @@ static void encrypt(char text[1024]) {
 }
 
 // Concatenates the user's name and the message, to ease sending to server and printing on other clients/broadcasting
-static char* concat(char message[1024], char username[30]) {
-    char* final_message;
-    strcat(final_message, username);
+static char* concat(const char message[512], const char username[30]) {
+    // Allocate enough memory: username + "> " + message + null terminator
+    size_t len = strlen(username) + 2 + strlen(message) + 1;
+    char* final_message = malloc(len);
+
+    if (!final_message) return NULL; // Allocation failed
+
+    strcpy(final_message, username);
     strcat(final_message, "> ");
     strcat(final_message, message);
 
-    // Output should look like: "username> The message that the user inputed"
+    final_message[len - 1] = '\0';
+
     return final_message;
 }
 
