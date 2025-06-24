54cdb019951269840db4dab94f79d953e8227900
francisco_correia47 <jfranciscosacorreia@gmail.com>
2025-06-24 12:46:25 -0100

    Update simple-chat_server.c
    Almost totally functional!

--- simple-chat_server.c
+++ simple-chat_server.c
@@ -1,128 +1,83 @@
 #include "simple-chat_functions.h"
 
-void forward_messages(Client* client);
-void* receive_messages(void* clientStruct);
+Client clients[MAX_CLIENTS];
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-char 			buff[5][MAX_MSG_SIZE];
-int 			bytes_read = 0;
-pthread_mutex_t accept_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-Client Clients[5];
+void* handle_client(void* arg);
+void broadcast(const char* msg, int sender_id);
 
 int main(void) {
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
 
-	// Starting up the WinSock API
-	WSADATA wsa;
-	WSAStartup(MAKEWORD(2, 2), &wsa);
+    SOCKET server = initialize_Socket_IPv4();
+    struct sockaddr_in server_addr = generate_IPv4_Address("", PORT);
 
-	pthread_t thread1[5];
+    bind(server, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    listen(server, MAX_CLIENTS);
 
-	// Initializing the Server Socket
-	SOCKET server = initialize_Socket_IPv4();
+    printf("Server listening on port %d...\n", PORT);
+    getWirelessIP();
 
-	// Generating the IPv4 address for use with the server
-	struct sockaddr_in serverAddress = generate_IPv4_Address("", PORT);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        clients[i].socket = INVALID_SOCKET;
+    }
 
-	// Binds the server's Socket and Address in the operating system
-	bind(server, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    int client_id = 0;
+    while (1) {
+        if (client_id >= MAX_CLIENTS) break;
 
-	// Get the server's Wireless IP to let the clients know where to connect
-	int checkIP = getWirelessIP();
-	if (checkIP) {
-		Sleep(2000);
-		return 1;
-	}
+        SOCKET new_client = accept(server, NULL, NULL);
+        if (new_client == INVALID_SOCKET) continue;
 
-	// Start listening for new connections
-	listen(server, 5);
+        clients[client_id].socket = new_client;
+        clients[client_id].id = client_id;
 
-	// Accept incoming client connections
-	int count = 0;
-	while (count < 5) {
-		for (int i = 0; i < 5; i++) {
-			Clients[i].socket = SOCKET_ERROR;
-			Clients[i].buffer = i;
-			if (Clients[i].socket != SOCKET_ERROR)
-				continue;
-			else {
-				// Lock to avoid trying to receive/send before even having clients connected
-				pthread_mutex_lock(&accept_mutex);
-				Clients[i].socket = accept(server, NULL, NULL);
-				pthread_create(&thread1[i], NULL, receive_messages, (void*)&Clients[i]);
-				pthread_mutex_unlock(&accept_mutex);
-				if (Clients[i].socket == SOCKET_ERROR) break;
-				printf("Client %d Connected\n", i+1);
-				count++;
-			}
-		}
-	}
+        pthread_create(&clients[client_id].thread, NULL, handle_client, (void*)&clients[client_id]);
 
+        printf("Client %d connected.\n", client_id + 1);
+        client_id++;
+    }
 
-	// Joining the thread so that the program waits until there is no traffic to close
-	for (int i = 0; i < 5; i++)
-		pthread_join(thread1[i], NULL);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        pthread_join(clients[i].thread, NULL);
+        closesocket(clients[i].socket);
+    }
 
-	// Closing all sockets
-	for (int i = 0; i < 5; i++)
-		closesocket(Clients[i].socket);
-	closesocket(server);
-
-	// WinSock API function that cleans the socket usage and other resources, to avoid safety issues
-	WSACleanup();
-
-	return 0;
-
+    closesocket(server);
+    WSACleanup();
+    return 0;
 }
 
-// This function receives the client struct parsed as a void*, and then typecasted back to a Client*
-void* receive_messages(void* clientStruct) {
+void* handle_client(void* arg) {
+    Client* client = (Client*)arg;
 
-	Client* client = (Client*)clientStruct;
+    while (1) {
+        memset(client->buffer, 0, MAX_MSG_SIZE);
+        int bytes_read = recv(client->socket, client->buffer, MAX_MSG_SIZE - 1, 0);
 
-	// Receives messages until no message was received
-	while (1) {
-			
-			// Cleaning the buffer to avoid memory trash
-			memset(buff[client->buffer], 0, MAX_MSG_SIZE);
-			pthread_mutex_lock(&accept_mutex);
-			// Ignore unused clients
-			if (client->socket == SOCKET_ERROR)
-				continue;
+        if (bytes_read <= 0 || strcmp(client->buffer, "/exit") == 0) {
+            printf("Client %d disconnected.\n", client->id + 1);
+            closesocket(client->socket);
+            client->socket = INVALID_SOCKET;
+            break;
+        }
 
-			bytes_read = recv(client->socket, buff[client->buffer], MAX_MSG_SIZE, 0);
+        client->buffer[bytes_read] = '\0';
+        broadcast(client->buffer, client->id);
+    }
 
-			// Check for disconnecting client
-			if (strcmp(buff[client->buffer], "/exit") == 0) {
-				client->socket = SOCKET_ERROR;
-				printf("Client %d Disconnected\n", client->buffer);
-				pthread_mutex_unlock(&accept_mutex);
-				break;
-			}
-			printf("Received %d bytes from Client %d\n", bytes_read, client->buffer);
-			pthread_mutex_unlock(&accept_mutex);
-
-		forward_messages(client);
-	}
-	return NULL;
+    return NULL;
 }
 
-// Sends a message to all the clients
-void forward_messages(Client* client){
+void broadcast(const char* msg, int sender_id) {
+    pthread_mutex_lock(&clients_mutex);
 
-	for (int i = 0; i < 5; i++) {
-		
-		// Cleaning the buffer to avoid memory trash
-		memset(buff[client->buffer], 0, MAX_MSG_SIZE);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].socket != INVALID_SOCKET && i != sender_id) {
+            send(clients[i].socket, msg, strlen(msg), 0);
+        }
+    }
 
-		// Locking here to avoid receiving at the same position while sending
-		pthread_mutex_lock(&accept_mutex);
-		// Not send to unused clients
-		if (Clients[i].socket == SOCKET_ERROR) continue;
-
-		if (bytes_read > 0) {
-			buff[client->buffer][bytes_read] = '\0';
-			send(Clients[i].socket, buff[client->buffer], bytes_read, 0);
-		}
-		pthread_mutex_unlock(&accept_mutex);
-	}
+    pthread_mutex_unlock(&clients_mutex);
 }
