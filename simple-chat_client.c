345d203073288ed48465e25f614306e8bbfca2fd
francisco_correia47 <jfranciscosacorreia@gmail.com>
2025-06-25 16:43:57 -0100

    Clean up simple-chat_client.c

--- simple-chat_client.c
+++ simple-chat_client.c
@@ -2,10 +2,6 @@
 
 char buffer[MAX_MSG_SIZE];
 char username[30];
-pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;#include "simple-chat_functions.h"
-
-char buffer[MAX_MSG_SIZE];
-char username[30];
 pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* receive_messages(void* socket_ptr);
@@ -91,89 +87,6 @@ void* receive_messages(void* socket_ptr) {
         encrypt(buffer);  // Decrypt
         pthread_mutex_lock(&print_mutex);
         printf("%s\n", buffer);
-        fflush(stdout);
-        pthread_mutex_unlock(&print_mutex);
-    }
-
-    return NULL;
-}
-
-void* receive_messages(void* socket_ptr);
-
-int main(void) {
-    WSADATA wsa;
-    WSAStartup(MAKEWORD(2, 2), &wsa);
-
-    char serverIp[INET_ADDRSTRLEN];
-
-    printf("Insert your username:\n> ");
-    scanf("%s", username);
-    flush_input();
-    
-    do{
-        printf("Insert the server's IP:\n> ");
-        scanf("%s", serverIp);
-        flush_input();  // Prevent leftover \n
-    
-        SOCKET server = initialize_Socket_IPv4();
-        struct sockaddr_in addr = generate_IPv4_Address(serverIp, PORT);
-    
-        if (connect(server, (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
-            printf("Connection failed.\n");
-            system("cls");
-        }
-    }while(connect(server, (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR);
-    
-    printf("Connected to server!\n");
-
-    pthread_t receiver_thread;
-    pthread_create(&receiver_thread, NULL, receive_messages, (void*)&server);
-
-    char message[MAX_MSG_SIZE];
-    char final_message[1054];
-    
-    while (1) {
-        printf("You> ");
-        pthread_mutex_lock(&print_mutex);
-        if (fgets(message, MAX_MSG_SIZE, stdin)) {
-            size_t len = strlen(message);
-            if (len > 0 && message[len - 1] == '\n') {
-                message[len - 1] = '\0';
-            }
-        }
-        pthread_mutex_unlock(&print_mutex);
-
-        final_message = concat(message, username);
-        
-        if (strcmp(message, "/exit") == 0) {
-            send(server, final_message, strlen(final_message), 0);
-            break;
-        }
-
-        //encrypt(message);
-        send(server, message, strlen(message), 0);
-        Sleep(100);
-    }
-
-    pthread_join(receiver_thread, NULL);
-    closesocket(server);
-    WSACleanup();
-    return 0;
-}
-
-void* receive_messages(void* socket_ptr) {
-    SOCKET* server = (SOCKET*)socket_ptr;
-
-    while (1) {
-        memset(buffer, 0, sizeof(buffer));
-        int bytes = recv(*server, buffer, MAX_MSG_SIZE - 1, 0);
-
-        if (bytes <= 0) break;
-
-        buffer[bytes] = '\0';
-        //encrypt(buffer);  // Decrypt
-        pthread_mutex_lock(&print_mutex);
-        printf("[Server]> %s\n", buffer);
         fflush(stdout);
         pthread_mutex_unlock(&print_mutex);
     }
